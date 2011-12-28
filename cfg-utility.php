faa89a78c602c9aa97b0df79627c82bb33f1422c
tinybeans <register@tinybeans.net>
2011-12-28 12:53:08 +0900

    customizeの途中からスタート！

--- cfg-utility.php
+++ cfg-utility.php
@@ -4,9 +4,9 @@
   Plugin Name: Custom Field Gui Utility
   Plugin URI: http://www.tinybeans.net/blog/download/wp-plugin/cfg-utility-3.html
   Description: WordPress 3.1 のカスタムフィールドを使いやすくするプラグイン「Custom Field GUI」のカスタマイズ版。Original plugin's author is <a href="http://rhymedcode.net">Joshua Sigar</a>.
-  Author: Customized by Tomohiro Okuwaki
+  Author: Tomohiro Okuwaki
   Author URI: http://www.tinybeans.net/blog/
-  Version: 3.1.3
+  Version: 3.1.4
   Customize: Tomohiro Okuwaki (http://www.tinybeans.net/blog/)
   Thanks: @hadakadenkyu <http://twitter.com/hadakadenkyu>
 -- This Plugin's Information --------------------------------
@@ -26,62 +26,31 @@ rc:custom_field_gui
 Licensed under the MIT License
 Copyright (c)  2005 Joshua Sigar
 
-Permission is hereby granted, free of charge, to any person
-obtaining a copy of this software and associated
-documentation files (the "Software"), to deal in the
-Software without restriction, including without limitation
-the rights to use, copy, modify, merge, publish,
-distribute, sublicense, and/or sell copies of the Software,
-and to permit persons to whom the Software is furnished to
-do so, subject to the following conditions:
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 
-The above copyright notice and this permission notice shall
-be included in all copies or substantial portions of the
-Software.
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY
-KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
-WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
-PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS
-OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
-OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
-OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
-SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
 // カスタムフィールドを入れるボックス名の指定 [start]
 $box_name = 'カスタムフィールド';
 // カスタムフィールドを入れるボックス名の指定 [ end ]
 
-function insert_head () {
-    $plugin_url = get_bloginfo('wpurl') . '/wp-content/plugins/custom-field-gui-utility/';
-    $head = <<< EOF
-        <link rel="stylesheet" href="{$plugin_url}facebox/facebox.css" type="text/css" media="all" />
-        <link rel="stylesheet" href="{$plugin_url}cfg-utility.css" type="text/css" media="all" />
-        <script type="text/javascript" src="{$plugin_url}facebox/facebox.js"></script>
-        <script type="text/javascript" src="{$plugin_url}cfg-utility.js"></script>
-EOF;
-    echo $head;
-}
 add_action ('admin_head','insert_head');
 
 include_once ('cfg-utility.class.php');
 require_once (ABSPATH . 'wp-admin/includes/template.php');
 
-/* post and page */
-add_action ('simple_edit_form', array ('cfg_utility_class', 'insert_gui'));
-  /* simple_edit_form:詳細設定のセクションが含まれない「シンプルモード」記事投稿フォームの終盤で実行する。デフォルトではシンプルモードフォームが使われるのはブックマークレットからの投稿のみ。*/
-
-/* post */
-add_meta_box('cfg_utility', $box_name, array('cfg_utility_class', 'insert_gui'), 'post', 'normal', 'high');
+add_action ('add_meta_boxes', 'isert_custom_field_gui');
 
 /* page and custom post type */
-$cur_post_type=$_REQUEST['post_type'];
-if($_REQUEST['post']){
+$cur_post_type = isset($_REQUEST['post_type']) ? $_REQUEST['post_type'] : '';
+if (isset($_REQUEST['post'])){
     $cur_post_type=get_post_type($_REQUEST['post']);
 }
 if ($cur_post_type){
-    add_meta_box('cfg_utility', $box_name, array('cfg_utility_class', 'insert_gui'), $cur_post_type, 'normal', 'high');
+    add_action('add_meta_boxes', 'isert_custom_field_gui');
 }
 
 /* post and page */
@@ -100,7 +69,112 @@ add_action( 'publish_post', array( 'cfg_utility_class', 'edit_meta_value' ) );
 add_action( 'transition_post_status', array( 'cfg_utility_class', 'edit_meta_value' ) );
   /* transition_post_status:バージョン2.3以上。記事・ページが公開された際、またはステータスが「公開」に変更された場合に実行する。*/
 
-/* Functions */
+/****************
+   Main Scripts
+ ****************/
+
+function insert_head () {
+    $plugin_url = get_bloginfo('wpurl') . '/wp-content/plugins/custom-field-gui-utility/';
+    $head = <<< EOD
+<link rel="stylesheet" href="{$plugin_url}facebox/facebox.css" type="text/css" media="all" />
+<link rel="stylesheet" href="{$plugin_url}cfg-utility.css" type="text/css" media="all" />
+<script type="text/javascript" src="{$plugin_url}facebox/facebox.js"></script>
+<script type="text/javascript" src="{$plugin_url}cfg-utility.js"></script>
+EOD;
+    echo $head;
+}
+
+function isert_custom_field_gui ($post_type = 'post', $post = NULL) {
+    add_meta_box('cfg_utility', get_field_title(FALSE), 'insert_gui', $post_type, 'normal', 'high');
+}
+
+function get_field_title ($enable) {
+    $file = dirname(__FILE__) . '/conf.ini';
+    $arry = file($file);
+    $title = preg_replace('/^;\s/', '', $arry[0]);
+    if (! $enable) {
+        $title = 'カスタムフィールド';
+    }
+    return $title;
+}
+
+function insert_gui () {
+    $out = '<srtong style="font-weight:bold;color:red;">Good Job !</srtong>';
+/*
+    $file_path = dirname(__FILE__) . '/conf' . $suffix . '.ini';
+    if (!file_exists($file)) {
+        return null;
+    }
+    $custom_fields = parse_ini_file($file, true);
+
+    $fields = cfg_utility_class::get_custom_fields();
+    if ($fields == null) {
+        return;
+    }
+    $out = '<input type="hidden" name="custom-field-gui-verify-key" id="custom-field-gui-verify-key"
+        value="' . wp_create_nonce('custom-field-gui') . '" />';
+
+    foreach ($fields as $title => $data) {
+        $cat_check = TRUE;
+        $post_type = 'post';
+        $post_id = isset($_REQUEST['post']) ? $_REQUEST['post'] : '';
+        if (isset($post_id)) {
+            $post_type = get_post_type($post_id);
+            if ($post_type == 'post' and isset($data['category']) and $cat_check) {
+                $cat_array = explode(' ', $data['category']);
+                $cats = get_the_category($post_id);
+                foreach ($cats as $cat) {
+                    $cat_slug = $cat->slug;
+                    if (in_array($cat_slug, $cat_array)) {
+                        $cat_check = FALSE;
+                    }
+                }
+                if ($cat_check) {
+                    continue;
+                }
+            }
+        } elseif ($_REQUEST['post_type']){
+            $post_type = $_REQUEST['post_type'];
+        }
+        $class_array = explode(' ',$data['class']);
+        if (!in_array($post_type, $class_array)) {
+            continue;
+        }
+        if ($data['type'] == 'textfield') {
+            $out .= cfg_utility_class::make_textfield($title, $data['type'], $data['class'], $data['default'], $data['size'], $data['sample'], $data['fieldname'], $data['must']);
+        } elseif ($data['type'] == 'imagefield') {
+            $out .= cfg_utility_class::make_imagefield($title, $data['type'], $data['class'], $data['size'], $data['sample'], $data['fieldname'], $data['must'], $data['idname']);
+        } elseif ($data['type'] == 'filefield') {
+            $out .= cfg_utility_class::make_filefield($title, $data['type'], $data['class'], $data['size'], $data['sample'], $data['fieldname'], $data['must'], $data['idname']);
+        } elseif ($data['type'] == 'checkbox') {
+            $out .= 
+                cfg_utility_class::make_checkbox($title, $data['type'], $data['class'], $data['default'], $data['sample'], $data['fieldname'], $data['must']);
+        } elseif ($data['type'] == 'multi_checkbox') {
+            $out .= 
+                cfg_utility_class::make_multi_checkbox($title, $data['type'], $data['class'], explode('#', $data['value']), $data['default'], $data['sample'], $data['fieldname'], $data['must']);
+        } elseif ($data['type'] == 'radio') {
+            $out .= 
+                cfg_utility_class::make_radio(
+                    $title, $data['type'], $data['class'], explode('#', $data['value']), $data['default'], $data['sample'], $data['fieldname'], $data['must']);
+        } elseif ($data['type'] == 'select') {
+            $out .= 
+                cfg_utility_class::make_select(
+                    $title, $data['type'], $data['class'], explode('#', $data['value']), $data['default'], $data['sample'], $data['fieldname'], $data['must']);
+        } elseif ($data['type'] == 'textarea') {
+            $out .= 
+                cfg_utility_class::make_textarea($title, $data['type'], $data['class'], $data['rows'], $data['cols'], $data['sample'], $data['fieldname'], $data['must']);
+        } elseif ($data['type'] == 'hr') {
+            $out .= 
+                cfg_utility_class::make_hr($data['class'], $data['fieldname']);
+        }
+    }
+*/
+    echo $out;
+}
+
+/*************
+   Functions
+ *************/
 function get_imagefield($key) {
     $imagefield = post_custom($key);
     $out['id'] = preg_replace('/(\[)([0-9]+)(\])(http.+)/', '$2', $imagefield);
